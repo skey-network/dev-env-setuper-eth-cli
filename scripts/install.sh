179cdc2a4bf8c1d29c31b412c28ba49f1703f923
Adrian <adrian.rom64@gmail.com>
2023-11-22 16:03:49 +0100

    asd

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# tmp=$(mktemp)
-
 mkdir -p ~/.local/bin
 
-cp bin/sk-eth-cli ~/.local/bin/sk-eth-cli
+curl -o ~/.local/bin/sk-eth-cli -L https://github.com/skey-network/dev-env-setuper-eth-cli/raw/main/bin/sk-eth-cli
+
+chmod +x ~/.local/bin/sk-eth-cli
+
+sk-eth-cli --help
